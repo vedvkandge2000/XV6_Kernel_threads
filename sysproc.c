cda34965bb2fc0a9fb22e3da875e536c8dd11b3f
pranavsadavarte221b <pranavsadavarte221b@gmail.com>
2021-04-08 23:13:56 +0530

    clone_function_added

--- sysproc.c
+++ sysproc.c
@@ -89,3 +89,20 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+int sys_clone(void)
+{
+  void (*fcn)(void *, void *);
+  void *arg1;
+  void *arg2;
+  void *stack;
+  if(argptr(0, (void*)&fcn, sizeof(void*)) < 0)
+    return -1;
+  if(argptr(1, (void*)&arg1, sizeof(void*)) < 0)
+    return -1;
+  if(argptr(2, (void*)&arg2, sizeof(void*)) < 0)
+    return -1;
+  if(argptr(3, (void*)&stack, sizeof(void*)) < 0)
+    return -1;
+  return clone(fcn, arg1, arg2, stack);
+}
